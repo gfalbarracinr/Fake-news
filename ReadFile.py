98e943af575b6af490f878e1d84ed302b0a3111e
gfalbarracinr <gfalbarracinr@unal.edu.co>
2019-02-18 09:40:34 +0100

    trying to refactor vocabulary script

--- ReadFile.py
+++ ReadFile.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from langdetect import detect
 from DataSetCleaner import DataSetCleaner
-
+from vocabulary import Vocabulary
 
 data = pd.read_csv('test.csv', delimiter=',')
 dataSetCleaner = DataSetCleaner
 
-data = dataSetCleaner.removeLanguage(DataSetCleaner, data, 'en')
-data = dataSetCleaner.removeHTML(DataSetCleaner, data)
-dataSetCleaner.writeToFile(DataSetCleaner, data, 'test.csv')
+#data = dataSetCleaner.removeLanguage(DataSetCleaner, data, 'en')
+#data = dataSetCleaner.removeHTML(DataSetCleaner, data)
+#dataSetCleaner.writeToFile(DataSetCleaner, data, 'test.csv')
+vocabulary = Vocabulary('fake_news_words.csv')
+totalWords = vocabulary.countWordsTotal(data)
+dataSetCleaner.writeToFile(dataSetCleaner, totalWords, 'words.csv')
+print(totalWords)
 
--- vocabulary.py
+++ vocabulary.py
@@ -3,34 +3,35 @@ import pandas as pd
 from string import punctuation
 import re
 
+class Vocabulary:
+    def __init__(self, fileName):
+        self.file = pd.read_csv(fileName, delimiter=',')
+        self.words = self.file['words']
 
-def countWords (data, words):
-    featureMatrix = {}
-    translator = str.maketrans('', '', punctuation)
-    for index, row in data.iterrows():
-        featureMatrix[index] = {}
-        article = str(row['text'])
-        for word in words:
-            (featureMatrix[index])[word] = article.translate(translator).lower().split().count(word)
-    return featureMatrix
+    def countWords (self, data):
+        featureMatrix = {}
+        translator = str.maketrans('', '', punctuation)
+        for index, row in data.iterrows():
+            featureMatrix[index] = {}
+            article = str(row['text'])
+            for word in self.words:
+                (featureMatrix[index])[word] = article.translate(translator).lower().split().count(word)
+        return featureMatrix
 
-def countWordsTotal (data, words): 
-    featureMatrix = {}
-    translator = str.maketrans('', '', punctuation)
-    for index, row in data.iterrows():
-        article = str(row['text'])
-        for word in words:
-            if word in featureMatrix:
-                featureMatrix[word] += article.translate(translator).lower().split().count(word)
-            else:
-                featureMatrix[word] = article.translate(translator).lower().split().count(word)
-    return featureMatrix
+    def countWordsTotal (self, data): 
+        featureMatrix = {}
+        translator = str.maketrans('', '', punctuation)
+        for index, row in data.iterrows():
+            article = str(row['text'])
+            cleanedArticle = article.translate(translator).lower().split()
+            for word in self.words:
+                if word in featureMatrix:
+                    featureMatrix[word] += cleanedArticle.count(word)
+                else:
+                    featureMatrix[word] = cleanedArticle.count(word)
+                
+        return featureMatrix
 
-data = pd.read_csv('testEnglish.csv', delimiter=',')
-word = ['elections', "trump" ]
-string = "this is random text and it's a test for the method a aa a. a' "
-newMatrix = countWordsTotal(data, word)
-print (newMatrix)
 
 
 
